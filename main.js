bed03756ee634d53d464d9c20f11b4e4603f4a84
Joshua Ho <ho.joshua4@gmail.com>
2020-08-03 13:03:17 -0700

    Refactored Report into a class

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Command Line Arguments
 const fs = require('fs')
 const dataService = require('./dataService');
+let Report = require('./studentService');
 
 //node main.js data/courses.csv data/students.csv data/tests.csv data/marks.csv output.json
 //Application Data
@@ -11,18 +12,21 @@ async function App(){
 
     const inputArgs = process.argv.slice(2);
     const outputFilename = inputArgs[4];
-    const outputData = {students: [] };
+    // const outputData = {students: [] };
 
     //Setup application data
     [coursesCache, studentsCache, testsCache, marksCache] = await dataService.createAppCache( inputArgs );
     
     //Construct output JSON file structure
-    for (const id in studentsCache){
-        let newStudent = makeStudent(id)
-        if (newStudent) outputData.students.push(newStudent);
-    }
+    // for (const id in studentsCache){
+    //     let newStudent = generateStudent(id)
+    //     if (newStudent) outputData.students.push(newStudent);
+    // }
 
-    //Validate information
+    let report = new Report(coursesCache, studentsCache, testsCache, marksCache);
+    let outputData = report.generate();
+    //Generate Data
+    // let outputData = generateStudentReport(coursesCache, studentsCache, testsCache, marksCache)
 
     //Write output.json
     dataService.writeFile(outputFilename, outputData);
@@ -31,78 +35,93 @@ async function App(){
 
 App();
 
-function makeStudent( id ){
+// function generateStudentReport(
+//     coursesCache, 
+//     studentsCache, 
+//     testsCache, 
+//     marksCache
+// ){
 
-    const studentData = studentsCache[id];
-    let totalAvg = 0;
+//     const reportData = {students: [] };
 
-    let newStudent = {
-        id: parseInt(id),
-        name: studentData.name,
-        totalAverage: 9999,
-        courses: generateClass(id)
-    }
+//     for (const id in studentsCache){
+//         let newStudent = generateStudent(id)
+//         if (newStudent) reportData.students.push(newStudent);
+//     }
+    
+//     return reportData;
+// }
 
-    //Ignore students that have no grades
-    if (!newStudent.courses) return;
+// function generateStudent( id ){
 
-    for (let i = 0; i < newStudent.courses.length; i++){
-        totalAvg += newStudent.courses[i].courseAverage;   
-    }
+//     const studentData = studentsCache[id];
+//     let totalAvg = 0;
 
-    totalAvg = Math.floor( (totalAvg * 100) / newStudent.courses.length) / 100;
+//     let newStudent = {
+//         id: parseInt(id),
+//         name: studentData.name,
+//         totalAverage: 9999,
+//         courses: generateClass(id)
+//     }
+
+//     //Ignore students that have no grades
+//     if (!newStudent.courses) return;
+
+//     for (let i = 0; i < newStudent.courses.length; i++){
+//         totalAvg += newStudent.courses[i].courseAverage;   
+//     }
+
+//     totalAvg = Math.floor( (totalAvg * 100) / newStudent.courses.length) / 100;
      
-    newStudent.totalAverage = totalAvg;
+//     newStudent.totalAverage = totalAvg;
     
-    return newStudent;
-}
+//     return newStudent;
+// }
+
+// function generateClass(studentId){
+//     //Creates a whole course
+
+//     let answer = [];
+//     let gradesCache = generateWeightedGrades(studentId);
+
+//     for( const classId in gradesCache){
+//         let data = {
+//             "id": parseInt(classId),
+//             "name": coursesCache[classId].name,
+//             "teacher": coursesCache[classId].teacher,
+//             "courseAverage": gradesCache[classId]/100
+//         }
+
+//         answer.push(data);
+//     }
+
+//     return answer;
+// }
 
 
-//returns course: combined weighted scores
-//function getStudentCourseAverage( studentId ){
-function getWeightedGrades( studentId ){
-    
-    //Creates weighted scores
-    //If the weight is >100 then return error
-    let gradesCache = {};
+// //returns course: combined weighted scores
+// //function getStudentCourseAverage( studentId ){
+// function generateWeightedGrades( studentId ){
 
-    let studentMarks = marksCache[studentId];
+//     //Creates weighted scores
+//     //If the weight is >100 then return error
+//     let gradesCache = {};
 
-    if (!studentMarks) return;
+//     let studentMarks = marksCache[studentId];
 
-    for (let i = 0; i < studentMarks.length; i++){
+//     if (!studentMarks) return;
+
+//     for (let i = 0; i < studentMarks.length; i++){
         
-        let testIdToFind = studentMarks[i].testId;
-        let test = testsCache[testIdToFind];
-        let grade = test.weight * studentMarks[i].mark;
-      
-        if (!gradesCache[test.course_id]) {
-            gradesCache[test.course_id] = grade
-        } else gradesCache[test.course_id] += grade   
-    }
+//         let testIdToFind = studentMarks[i].testId;
+//         let test = testsCache[testIdToFind];
+//         let grade = test.weight * studentMarks[i].mark;
+        
+//         if (!gradesCache[test.course_id]) {
+//             gradesCache[test.course_id] = grade
+//         } else gradesCache[test.course_id] += grade   
+//     }
 
-    return gradesCache;
+//     return gradesCache;
 
-}
-
-function generateClass(studentId){
-    //Creates a whole course
-
-    let answer = [];
-    let gradesCache = getWeightedGrades(studentId);
-
-    for( const classId in gradesCache){
-        let data = {
-            "id": parseInt(classId),
-            "name": coursesCache[classId].name,
-            "teacher": coursesCache[classId].teacher,
-            "courseAverage": gradesCache[classId]/100
-        }
-
-        answer.push(data);
-    }
-
-    return answer;
-}
-
-
+// }
--- studentService.js
+++ studentService.js
@@ -1,9 +1,191 @@
-class StudentService{
-    constructor(cache){
-        this.cache = cache;
-        console.log('my life for Aiur', cache);
+class Report{
+    constructor( 
+        coursesCache, 
+        studentsCache, 
+        testsCache, 
+        marksCache
+    ){
+        this.coursesCache = coursesCache;
+        this.studentsCache = studentsCache;
+        this.testsCache = testsCache;
+        this.marksCache = marksCache;
+    }
+    generate(){
+        const reportData = {students: [] };
+
+        for (const id in this.studentsCache){
+            let newStudent = this.generateStudent(id)
+            if (newStudent) reportData.students.push(newStudent);
+        }
+    
+        return reportData;
     }
 
-}
+    generateStudent( id ){
 
-module.exports = StudentService;
+        const studentData = this.studentsCache[id];
+        let totalAvg = 0;
+    
+        let newStudent = {
+            id: parseInt(id),
+            name: studentData.name,
+            totalAverage: 9999,
+            courses: this.generateClass(id)
+        }
+    
+        //Ignore students that have no grades
+        if (!newStudent.courses) return;
+    
+        for (let i = 0; i < newStudent.courses.length; i++){
+            totalAvg += newStudent.courses[i].courseAverage;   
+        }
+    
+        totalAvg = Math.floor( (totalAvg * 100) / newStudent.courses.length) / 100;
+         
+        newStudent.totalAverage = totalAvg;
+        
+        return newStudent;
+    }
+
+    generateClass(studentId){
+
+        //Creates a whole course
+        let answer = [];
+        let tempGradesCache = this.generateWeightedGrades(studentId);
+    
+        for( const classId in tempGradesCache){
+            let data = {
+                "id": parseInt(classId),
+                "name": this.coursesCache[classId].name,
+                "teacher": this.coursesCache[classId].teacher,
+                "courseAverage": tempGradesCache[classId]/100
+            }
+    
+            answer.push(data);
+        }
+    
+        return answer;
+    }
+
+    generateWeightedGrades( studentId ){
+
+        //Creates weighted scores
+        //If the weight is >100 then return error
+        let tempGradesCache = {};
+    
+        let studentMarks = this.marksCache[studentId];
+    
+        if (!studentMarks) return;
+    
+        for (let i = 0; i < studentMarks.length; i++){
+            
+            let testIdToFind = studentMarks[i].testId;
+            let test = this.testsCache[testIdToFind];
+            let grade = test.weight * studentMarks[i].mark;
+            
+            if (!tempGradesCache[test.course_id]) {
+                tempGradesCache[test.course_id] = grade
+            } else tempGradesCache[test.course_id] += grade   
+        }
+    
+        return tempGradesCache;
+    
+    }
+
+}//end
+
+
+// function generateStudentReport(
+//     coursesCache, 
+//     studentsCache, 
+//     testsCache, 
+//     marksCache
+// ){
+
+//     const reportData = {students: [] };
+
+//     for (const id in studentsCache){
+//         let newStudent = generateStudent(id)
+//         if (newStudent) reportData.students.push(newStudent);
+//     }
+    
+//     return reportData;
+// }
+
+// function generateStudent( id ){
+
+//     const studentData = studentsCache[id];
+//     let totalAvg = 0;
+
+//     let newStudent = {
+//         id: parseInt(id),
+//         name: studentData.name,
+//         totalAverage: 9999,
+//         courses: generateClass(id)
+//     }
+
+//     //Ignore students that have no grades
+//     if (!newStudent.courses) return;
+
+//     for (let i = 0; i < newStudent.courses.length; i++){
+//         totalAvg += newStudent.courses[i].courseAverage;   
+//     }
+
+//     totalAvg = Math.floor( (totalAvg * 100) / newStudent.courses.length) / 100;
+     
+//     newStudent.totalAverage = totalAvg;
+    
+//     return newStudent;
+// }
+
+
+// function generateClass(studentId){
+//     //Creates a whole course
+
+//     let answer = [];
+//     let gradesCache = generateWeightedGrades(studentId);
+
+//     for( const classId in gradesCache){
+//         let data = {
+//             "id": parseInt(classId),
+//             "name": coursesCache[classId].name,
+//             "teacher": coursesCache[classId].teacher,
+//             "courseAverage": gradesCache[classId]/100
+//         }
+
+//         answer.push(data);
+//     }
+
+//     return answer;
+// }
+
+
+//returns course: combined weighted scores
+//function getStudentCourseAverage( studentId ){
+// function generateWeightedGrades( studentId ){
+
+//     //Creates weighted scores
+//     //If the weight is >100 then return error
+//     let gradesCache = {};
+
+//     let studentMarks = marksCache[studentId];
+
+//     if (!studentMarks) return;
+
+//     for (let i = 0; i < studentMarks.length; i++){
+        
+//         let testIdToFind = studentMarks[i].testId;
+//         let test = testsCache[testIdToFind];
+//         let grade = test.weight * studentMarks[i].mark;
+        
+//         if (!gradesCache[test.course_id]) {
+//             gradesCache[test.course_id] = grade
+//         } else gradesCache[test.course_id] += grade   
+//     }
+
+//     return gradesCache;
+
+// }
+
+
+module.exports = Report;
